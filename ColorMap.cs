b9d91e43ff3bb66ae2c0f6e6cd0d40a5f6c18b33
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-17 18:42:11 +0530

    Update ColorMap.cs

--- ColorMap.cs
+++ ColorMap.cs
@@ -43,21 +43,4 @@ namespace TelCo.ColorCoder
             return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
         }
     }
-
-    public class ColorPair
-    {
-        public Color MajorColor { get; }
-        public Color MinorColor { get; }
-
-        public ColorPair(Color majorColor, Color minorColor)
-        {
-            MajorColor = majorColor;
-            MinorColor = minorColor;
-        }
-
-        public override string ToString()
-        {
-            return $"MajorColor:{MajorColor.Name}, MinorColor:{MinorColor.Name}";
-        }
-    }
 }
