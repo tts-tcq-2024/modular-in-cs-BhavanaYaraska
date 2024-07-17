68e36420dee0f9ba475fc2548bade6ccb5a7f267
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-17 18:41:37 +0530

    Create ColorPair.cs

---
+++ ColorPair.cs
@@ -0,0 +1,16 @@
+ public class ColorPair
+    {
+        public Color MajorColor { get; }
+        public Color MinorColor { get; }
+
+        public ColorPair(Color majorColor, Color minorColor)
+        {
+            MajorColor = majorColor;
+            MinorColor = minorColor;
+        }
+
+        public override string ToString()
+        {
+            return $"MajorColor:{MajorColor.Name}, MinorColor:{MinorColor.Name}";
+        }
+    }
